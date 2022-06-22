6ac815b47734a42e06ee8e4ab65381bf94d3164b
Haftas Arden <haftasarden@proton.me>
2022-06-22 01:03:28 -0300

    feat: adiciona código para verificar a integridade dos dados
    O código irá comparar o número de seguidores, comentários e views e o shortcode da publicação
    
    A comparação será do mesmo dias nos arquivos posts.json e posts_original.json

---
+++ check_integrity.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for username in $(ls -1 ./data); do
+    for selected_date in $(ls -1 ./data/$username); do
+        posts_original=$(cat ./data/$username/$selected_date/posts_original.json | jq '.[].node | {code: .shortcode, likes: .edge_media_preview_like.count, comments: .edge_media_to_comment.count}')
+        posts=$(cat ./data/$username/$selected_date/posts.json | jq '.[] | {code: .shortcode, likes: .totals.likes, comments: .totals.comments}')
+
+        posts_original_hash=$(sha1sum <<< $posts_original)
+        posts_hash=$(sha1sum <<< $posts)
+
+        if [[ $posts_original_hash == $posts_hash ]]; then
+            echo "Files OK"
+        else
+            echo "Failed: $username -> $selected_date"
+            git diff $(git hash-object -w --stdin <<< $posts_original) $(git hash-object -w --stdin <<< $posts)
+        fi
+    done
+done
