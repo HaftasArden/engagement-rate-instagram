7f8e1bc86e1fb53967a8808cf6512e3a5ab178d5
Haftas Arden <haftasarden@proton.me>
2022-06-22 01:04:32 -0300

    feat: adiciona script para executar, automaticamente, o script "generate_json.js"

---
+++ scripts/bbb22.sh
@@ -0,0 +1,28 @@
+sleep 5 && echo "Perfil: @arthuraguiar" && node ../generate_json.js arthuraguiar
+sleep 5 && echo "Perfil: @ba.heck" && node ../generate_json.js ba.heck
+sleep 5 && echo "Perfil: @brunnagoncalves" && node ../generate_json.js brunnagoncalves
+sleep 5 && echo "Perfil: @douglassilva" && node ../generate_json.js douglassilva
+sleep 5 && echo "Perfil: @eliezer" && node ../generate_json.js eliezer
+sleep 5 && echo "Perfil: @eslomarques" && node ../generate_json.js eslomarques
+sleep 5 && echo "Perfil: @gustavo_marsengo" && node ../generate_json.js gustavo_marsengo
+
+echo -e "\nAguarde 60 segs\n\n" && sleep 60
+
+sleep 5 && echo "Perfil: @jadepicon" && node ../generate_json.js jadepicon
+sleep 5 && echo "Perfil: @jessilane" && node ../generate_json.js jessilane
+sleep 5 && echo "Perfil: @laiscaldass" && node ../generate_json.js dra.laiscaldass
+sleep 5 && echo "Perfil: @larissatomasia" && node ../generate_json.js larissatomasia
+sleep 5 && echo "Perfil: @linndaquebrada" && node ../generate_json.js linndaquebrada
+sleep 5 && echo "Perfil: @bissolilucas" && node ../generate_json.js bissolilucas
+sleep 5 && echo "Perfil: @lucianoestevan" && node ../generate_json.js lucianoestevan
+sleep 5 && echo "Perfil: @eumaria" && node ../generate_json.js eumaria
+
+echo -e "\nAguarde 60 segs\n\n" && sleep 60
+
+sleep 5 && echo "Perfil: @naiaraazevedo" && node ../generate_json.js naiaraazevedo
+sleep 5 && echo "Perfil: @deodato" && node ../generate_json.js natalia.deodato
+sleep 5 && echo "Perfil: @iampauloandre" && node ../generate_json.js iampauloandre
+sleep 5 && echo "Perfil: @pedroscooby" && node ../generate_json.js pedroscooby
+sleep 5 && echo "Perfil: @mussi" && node ../generate_json.js rodrigo.mussi
+sleep 5 && echo "Perfil: @tiagoabravanel" && node ../generate_json.js tiagoabravanel
+sleep 5 && echo "Perfil: @vyniof" && node ../generate_json.js vyniof
